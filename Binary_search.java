7e1b290baff7b5e500246264f608b11953261bf2
Yokesh S <yokeshsampath25@gmail.com>
2026-07-09 20:35:42 +0530

    Implemented a binary search algorithm to find the target in a sorted array.
    Searching a target element from an array using binary search algorithm.

---
+++ Binary_search.java
@@ -0,0 +1,20 @@
+//Leetcode problem no-704
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else if(nums[mid]>target){
+                j=mid-1;
+            }
+        }
+        return -1;
+    }
+}
